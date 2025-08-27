02972b70eef306bddbac3ebd194e077cba969278
Dhinesh Waran P <dhineshwaran1712@gmail.com>
2025-08-27 11:58:46 +0530

    Update userRouter.js

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {Register, Login, logout, updateMe, getAllUsers, getOtherUsers} = require('../Controllers/UserControllers');
+const {Register, Login, logout, me, updateMe, getAllUsers, getOtherUsers} = require('../Controllers/UserControllers');
 const  userMiddleware  = require('../Middleware/userMiddleware')
 
 router.post('/signup' , Register );
 router.post('/signin' , Login );
 router.post("/logout", logout);
-router.put("/me", userMiddleware, updateMe);
+router.get("/details/:id", userMiddleware, me);
+router.put("/me/:id", userMiddleware, updateMe);
 router.get("/all", userMiddleware, getAllUsers);
 router.get("/others", userMiddleware, getOtherUsers);
 
